712c336785afc23558f1bd6e5794a9909ee7d510
ivanilieviliev <ivaniliev@schoolmath.eu>
2022-04-13 17:03:49 +0300

    Initial commit

---
+++ DataLayer/CustomerContext.cs
@@ -0,0 +1,105 @@
+﻿using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataLayer
+{
+    public class CustomerContext : IDB<Customer, int>
+    {
+        private OnlineShopDBContext _context;
+
+        public CustomerContext(OnlineShopDBContext context)
+        {
+            this._context = context;
+        }
+
+        public void Create(Customer item)
+        {
+            try
+            {
+                List<Product> products = new List<Product>(item.FavouriteProducts.Count());
+
+                foreach (Product product in item.FavouriteProducts)
+                {
+                    Product productFromDB = _context.Products.Find(product.Barcode);
+
+                    if (productFromDB != null)
+                    {
+                        products.Add(productFromDB);
+                    }
+                    else
+                    {
+                        products.Add(product);
+                    }
+                }
+
+                item.FavouriteProducts = products;
+
+                _context.Customers.Add(item);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Customer Read(int key)
+        {
+            try
+            {
+                return _context.Customers.Find(key);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IEnumerable<Customer> ReadAll()
+        {
+            try
+            {
+                return _context.Customers.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Update(Customer item)
+        {
+            try
+            {
+                Customer customerFromDB = Read(item.ID);
+
+                _context.Entry(customerFromDB).CurrentValues.SetValues(item);
+
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Delete(int key)
+        {
+            try
+            {
+                Customer customerFromDB = Read(key);
+
+                _context.Customers.Remove(customerFromDB);
+
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
